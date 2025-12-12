f929ee5a4686c8c70b7848bd9688048a7d12400f
Melany Carvajal Gomez <melanysofi2006@gmail.com>
2025-12-11 23:00:17 -0600

    Avance 4

--- Backend/src/models/product.model.js
+++ Backend/src/models/product.model.js
@@ -4,32 +4,37 @@
 const mongoose = require('mongoose');
 
 // Define la estructura de un producto en la base de datos
-const productSchema = new mongoose.Schema({
-  nombre: {
-    type: String,
-    required: true
+const productSchema = new mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+    },
+    precio: {
+      type: Number,
+      required: true,
+    },
+    cantidad: {
+      type: Number,
+      required: true,
+    },
+    descripcion: {
+      type: String,
+      default: '',
+    },
+    // Campo para BORRADO SUAVE
+    activo: {
+      type: Boolean,
+      default: true, // por defecto los productos están activos
+    },
   },
-  precio: {
-    type: Number,
-    required: true
-  },
-  cantidad: {
-    type: Number,
-    required: true
-  },
-  descripcion: {
-    type: String,
-    default: ''
-  },
-  activo: {
-    type: Boolean,
-    default: true // para borrado suave
+  {
+    timestamps: true, // createdAt y updatedAt
   }
-}, {
-  timestamps: true // createdAt y updatedAt
-});
+);
 
 // Crea el modelo basado en el esquema
 const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
+
